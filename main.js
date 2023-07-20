8714f4141684d6790439e04a7debbcca7698f4da
Daniel Guerrero <dansan_2002@hotmail.com>
2023-07-20 12:10:18 -0500

    terminamos la cuarta clase fusionando tanto en desktop como mobile el menu desplegable del carrito de compras, ademas de desarrollar la logica de las funciones para detectar si uno de ellos esta abiertos cerrar el otro para evitar solapamiento, se prevee un posible error con la clase 'aside' del carrito de compras, ya que se usa a posteriori para otros elementos, en espera de ver como solucionar el problema en futuras clases.

--- main.js
+++ main.js
@@ -2,16 +2,45 @@ const MenuEmail = document.querySelector(".navbar-email");
 const DesktopMenu = document.querySelector(".desktop-menu");
 const BurguerMenu = document.querySelector(".menu");
 const MobileMenu = document.querySelector(".mobile-menu");
+const CarritoIcon = document.querySelector(".navbar-shopping-cart");
+const CarroShopp = document.querySelector(".product-detail");
 
 /*despliegue del menu de Desktop*/
 MenuEmail.addEventListener("click", toggleDesktopMenu);
 /*despliegue del menu en mobile*/
 BurguerMenu.addEventListener("click", toggleMobileMenu);
+/*despliegue carro de compras */
+CarritoIcon.addEventListener("click", toggleCarShop);
 
 function toggleDesktopMenu() {
+	const isCarroShoppClosed = CarroShopp.classList.contains("inactive");
+
+	if (!isCarroShoppClosed) {
+		CarroShopp.classList.add("inactive");
+	}
 	DesktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
+	const isCarroShoppClosed = CarroShopp.classList.contains("inactive");
+
+	if (!isCarroShoppClosed) {
+		CarroShopp.classList.add("inactive");
+	}
+
 	MobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarShop() {
+	const isMobileMenuClosed = MobileMenu.classList.contains("inactive");
+	const isDesktopMenuClosed = DesktopMenu.classList.contains("inactive");
+
+	if (!isMobileMenuClosed) {
+		MobileMenu.classList.add("inactive");
+	}
+	if (!isDesktopMenuClosed) {
+		DesktopMenu.classList.add("inactive");
+	}
+
+	CarroShopp.classList.toggle("inactive");
 }
