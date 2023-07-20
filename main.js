daf1e52a3f1a09749a6b85e1ab5661685c967bb1
Daniel Guerrero <dansan_2002@hotmail.com>
2023-07-20 11:23:24 -0500

    termina la tercera clase con la fusion de menu mobile con el media query del navbar, ademas de crear un nuevo media query el cual oculte los menus segun si se esta en mobile o en desktop, ademas se soluciono un conflicto con el CSS del menu mobile que tenia con la funcion y la clase inactive, quitando su display flex y ayudandonos de un margin top para poder separar el correo y el sing out del resto de elementos del menu como se hacia antes con un display flex

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const MenuEmail = document.querySelector(".navbar-email");
 const DesktopMenu = document.querySelector(".desktop-menu");
+const BurguerMenu = document.querySelector(".menu");
+const MobileMenu = document.querySelector(".mobile-menu");
+
 /*despliegue del menu de Desktop*/
 MenuEmail.addEventListener("click", toggleDesktopMenu);
+/*despliegue del menu en mobile*/
+BurguerMenu.addEventListener("click", toggleMobileMenu);
+
 function toggleDesktopMenu() {
 	DesktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+	MobileMenu.classList.toggle("inactive");
 }
